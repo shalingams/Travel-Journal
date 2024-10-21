310f2e6a5232555ddd7883e55e4b8ca5c9d6a6e0
SHALINGA MANASINGHE <shalingams@gmail.com>
2024-10-21 18:37:21 +0300

    cleaning sign up

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,4 +1,79 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function SignUp() {
+  const [fullName, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event: any) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleFullNameChange = (event: any) => {
+    setFullName(event.target.value);
+  };
+
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordMatch = (): boolean => {
+    setPasswordError("");
+    if (password !== confirmPassword) {
+      setPasswordError("Password does not match");
+      return false;
+    }
+    return true;
+  };
+
+  const validatePassword = (): boolean => {
+    let passwordPattern =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (password.length < 8) {
+      setPasswordError("Minimum password length is 8");
+      return false;
+    }
+
+    if (!passwordPattern.test(password)) {
+      setPasswordError("Password is not complicated enough");
+      return false;
+    }
+    return passwordMatch();
+  };
+
+  const validateInput = ():boolean => {
+    if (fullName.length === 0){
+      setError("Full name is required");
+      return false
+    } else if (username.length === 0) {
+      setError("Username is required");
+      return false;
+    } else if (password.length === 0) {
+      setError("Password is required");
+      return false;
+    } else {
+      setError("")
+    }
+    return true;
+  }
+
+  const registration = () => {
+    if (validateInput() && validatePassword()) {
+      window.localStorage.setItem("tj-username", username);
+      navigate("/profile");
+    }
+  };
+
   return (
     <div className="font-Overlock bg-sky-100 flex justify-center items-center h-screen">
       <div className="w-1/2 h-screen hidden lg:block">
@@ -11,6 +86,11 @@ function SignUp() {
       <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
         <h1 className="text-4xl font-semibold mb-4 uppercase">Sign Up</h1>
         <form action="#" method="POST">
+          <div className="mb-4">
+            {error && (
+              <span className="text-md text-red-800 italic">{error}</span>
+            )}
+          </div>
           <div className="mb-4 bg-sky-100">
             <label htmlFor="full_name" className="block text-gray-600">
               Full name
@@ -21,6 +101,8 @@ function SignUp() {
               name="full_name"
               className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
               autoComplete="off"
+              value={fullName}
+              onChange={handleFullNameChange}
             />
           </div>
           <div className="mb-4 bg-sky-100">
@@ -33,7 +115,16 @@ function SignUp() {
               name="username"
               className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
               autoComplete="off"
+              value={username}
+              onChange={handleUsernameChange}
             />
+          </div>
+          <div className="mb-4">
+            {passwordError && (
+              <span className="text-sm text-red-800 italic">
+                {passwordError}
+              </span>
+            )}
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-800">
@@ -45,6 +136,7 @@ function SignUp() {
               name="password"
               className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
               autoComplete="off"
+              onChange={handlePasswordChange}
             />
           </div>
           <div className="mb-4">
@@ -57,11 +149,13 @@ function SignUp() {
               name="confirm_password"
               className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
               autoComplete="off"
+              onChange={handleConfirmPasswordChange}
             />
           </div>
           <button
-            type="submit"
+            type="button"
             className="bg-red-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+            onClick={registration}
           >
             Sign up
           </button>
