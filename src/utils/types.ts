c80bda9cf37d4a4f5d0551fcf0caa0e5bead30ea
SHALINGA MANASINGHE <shalingams@gmail.com>
2024-10-23 20:00:09 +0300

    Adding some types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,21 @@ export interface PostType {
   location: string;
   description: string;
 }
+
+export interface User {
+  name: string;
+  location: string;
+  current_location: string;
+  instagram: string;
+  facebook: string;
+  tiktok: string;
+  image: string;
+}
+
+export interface Contact {
+  name: string;
+  country: string;
+  town: string;
+  description: string;
+  contact: string;
+}
