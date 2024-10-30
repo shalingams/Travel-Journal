4c345c116fee3a360167aa415058ba81005f89e4
SHALINGA MANASINGHE <shalingams@gmail.com>
2024-10-30 20:08:16 +0200

    adding likes section

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,7 +2,7 @@ import { PostType } from "../utils/types";
 
 function Post(post: PostType) {
   const { image, description, location, likes } = post;
-  console.log(post);
+
   return (
     <div className="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
       <img
@@ -16,7 +16,7 @@ function Post(post: PostType) {
           {location}
         </h1>
         <p className="text-gray-700 my-2 hover-text-900 ">{description}</p>
-        <p>
+        <div className="flex gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -39,8 +39,8 @@ function Post(post: PostType) {
           >
             <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
           </svg>
-          {likes}
-        </p>
+          <p className="uppercase">{likes}</p>
+        </div>
       </div>
     </div>
   );
