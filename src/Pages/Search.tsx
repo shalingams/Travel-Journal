209d23dd34fcbeb802b940de0264f6831653bf0e
SHALINGA MANASINGHE <shalingams@gmail.com>
2024-12-23 12:32:53 +0200

    adding background

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -71,7 +71,7 @@ function Search() {
         <a href="/users/shalinga">
           <div className="max-w-[720px] mx-auto">
             <div className="relative grid h-[40rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
-              <div className="absolute inset-0 m-0 h-full w-full overflow-hidden bg-transparent bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
+              <div className="absolute inset-0 m-0 h-full w-full overflow-hidden bg-transparent bg-[url('https://travel-journal-8jd.pages.dev/data/3.jpg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
                 <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-black/80 via-black/50 to-bg-black-10"></div>
               </div>
               <div className="relative p-6 px-6 py-14 md:px-12">
